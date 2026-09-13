df27b2e0dd367e87eb1c429e31b9fdf1639aaa27
jenia109 <larchenko.jenia@yandex.ru>
2026-09-13 13:08:22 +0400

    Fix flake8: blank lines and line length

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,16 +1,12 @@
 from http import HTTPStatus
+
 from api import models
-from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 
-User = get_user_model()
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
-        self.user = User.objects.create_user(username='test_user', password='123')
-        self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
 
     def test_list_exists(self):
         """Проверка доступности списка задач."""
@@ -18,8 +14,11 @@ class TaskiAPITestCase(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи авторизованным пользователем."""
-        data = {'title': 'Test', 'description': 'Test'}
-        response = self.authorized_client.post('/api/tasks/', data=data)
+        """Проверка создания задачи."""
+        data = {
+            'title': 'Test',
+            'description': 'Test'
+        }
+        response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
